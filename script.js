84a7abee1aaf1ab39b1b212d58ce70d673d01715
Eric Chan <ericchan024@gmail.com>
2022-09-29 17:50:09 -0400

    renamed files

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, x, y) {
+    switch(operator) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+           return divide(x, y);
+    }
+}
