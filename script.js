eef36768249bc764e8058e7e4f7e8a6ff363674b
Eric Chan <ericchan024@gmail.com>
2022-10-05 18:25:40 -0400

    added functionality to dynamically resize when numbers get too large

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-/*
-Things to work on
-- dynamic resizing of numbers
-- scientific notation
-*/
-
 // Globals
 let output = 0;
 let x;
@@ -26,23 +20,6 @@ const buttons = document.querySelectorAll('button');
 
 getLastKeyClicked();
 
-// keyboard support
-window.addEventListener("keydown", (e) => {
-    console.log(e.key);
-    // numbers 1-9 input
-    if (e.key === 0) document.getElementById('zero').click();
-    if (e.key > 0 || e.key <= 9) document.getElementById(`${e.key}`).click();
-    if (e.key === "Backspace") document.getElementById("clear").click();
-    if (e.key === '_') document.getElementById("plusMinus").click();
-    if (e.key === "%") document.getElementById("percent").click();
-    if (e.key === "/") document.getElementById("divide").click();
-    if (e.key === "*") document.getElementById("mulitply").click();
-    if (e.key === "-") document.getElementById("subtract").click();
-    if (e.key === "+") document.getElementById("add").click();
-    if (e.key === "Enter") document.getElementById("equal").click();
-    if (e.key === ".") document.getElementById("decimal").click();
-});
-
 // button click events
 numberBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
@@ -55,38 +32,87 @@ numberBtn.forEach((btn) => {
 operatorBtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         e.target.focus();
-        
-        // if same operator is pressed, we ignore input
-        if (lastKeyPressed == currKeyPressed) return;
-
-        // Only one of the two following if's will be evaluated
-        // if the previous key and current key are both operators, we update
-        // the operator to the current operator
-        if (isOperator(lastKeyPressed) && isOperator(currKeyPressed)) {
-            operator = currKeyPressed;
-        // if the current key pressed is an operator and the last key press is not
-        // and there already exists an operator, then we will evaluate the 
-        // current expression
-        } else if (operator !== undefined && isOperator(currKeyPressed)) {
-            y = parseFloat(output);
-            output = operate(operator, x, y);
-            updateDisplay();
-        }
-
-        // after clicking operator to evaluate, this will allow user to change the 
-        // current operator
-        if (isOperator(lastKeyPressed) && isOperator(currKeyPressed)) {
-            operator = currKeyPressed;
-        // otherwise, process the number
-        } else {
-            x = parseFloat(output);
-            operator = currKeyPressed;
-            output = "";
-        }
+        getOperator();
     });
 });
 
 equals.addEventListener('click', () => {
+    evaluate();
+});
+
+clear.addEventListener('click', () => {
+    reset();
+    updateDisplay();
+});
+
+decimal.addEventListener('click', () => {
+    if (output.toString().includes(".")) {
+        return;
+    } else {
+        append('.');
+        updateDisplay();
+    }
+});
+
+plusMinus.addEventListener('click', () => {
+    output *= -1;
+    updateDisplay();
+});
+
+percent.addEventListener('click', () => {
+    output = (output / 100);
+    updateDisplay();
+})
+
+// keyboard support
+window.addEventListener("keydown", (e) => {
+    keyboardInput(e);
+});
+
+// helper functions
+function append(n) {
+    if (output === 0) {
+        if (n === '.') {
+            output += n;
+        } else {
+            // shows only 1 zero on screen when output is 0 and user presses 0
+            output = parseInt(output) + parseInt(n);
+        }
+    } else {
+        output += n;
+    }
+}
+
+function getLastKeyClicked() {
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            lastKeyPressed = currKeyPressed;
+            currKeyPressed = e.target.textContent;
+            // console.log(`Last: ${lastKeyPressed} \nCurr: ${currKeyPressed}`);
+        });
+    });
+}
+
+function updateDisplay() {
+    display.textContent = parseFloat(output);
+    resize();
+}
+
+function isOperator(a) {
+    switch(a) {
+        case '+':
+            return true;
+        case '-':
+            return true;
+        case '*':
+            return true;
+        case '/':
+            return true;
+    }
+    return false;
+}
+
+const evaluate = function() {
     // if there's no second value inputted after operator, the = key
     // assumes the second value is the same as the first
     if (isOperator(lastKeyPressed)) {
@@ -118,9 +144,39 @@ equals.addEventListener('click', () => {
         updateDisplay();
         operator = undefined;
     }
-});
+}
 
-clear.addEventListener('click', () => {
+const getOperator = function() {
+    // if same operator is pressed, we ignore input
+    if (lastKeyPressed == currKeyPressed) return;
+
+    // Only one of the two following if's will be evaluated
+    // if the previous key and current key are both operators, we update
+    // the operator to the current operator
+    if (isOperator(lastKeyPressed) && isOperator(currKeyPressed)) {
+        operator = currKeyPressed;
+    // if the current key pressed is an operator and the last key press is not
+    // and there already exists an operator, then we will evaluate the 
+    // current expression
+    } else if (operator !== undefined && isOperator(currKeyPressed)) {
+        y = parseFloat(output);
+        output = operate(operator, x, y);
+        updateDisplay();
+    }
+
+    // after clicking operator to evaluate, this will allow user to change the 
+    // current operator
+    if (isOperator(lastKeyPressed) && isOperator(currKeyPressed)) {
+        operator = currKeyPressed;
+    // otherwise, process the number
+    } else {
+        x = parseFloat(output);
+        operator = currKeyPressed;
+        output = "";
+    }
+}
+
+const reset = function() {
     output = 0;
     x = undefined;
     y = undefined;
@@ -129,68 +185,31 @@ clear.addEventListener('click', () => {
     operator = undefined;
     currKeyPressed = undefined;
     lastKeyPressed = undefined;
-    updateDisplay();
-});
+}
 
-decimal.addEventListener('click', () => {
-    if (output.toString().includes(".")) {
-        return;
+const keyboardInput = function(e) {
+    if (e.key === '0') document.getElementById("zero").click();
+    if (e.key > 0 && e.key <= 9) document.getElementById(`${e.key}`).click();
+    if (e.key === "Backspace") document.getElementById("clear").click();
+    if (e.key === '_') document.getElementById("plusMinus").click();
+    if (e.key === "%") document.getElementById("percent").click();
+    if (e.key === "/") document.getElementById("divide").click();
+    if (e.key === "*") document.getElementById("mulitply").click();
+    if (e.key === "-") document.getElementById("subtract").click();
+    if (e.key === "+") document.getElementById("add").click();
+    if (e.key === "Enter") document.getElementById("equal").click();
+    if (e.key === ".") document.getElementById("decimal").click();
+}
+
+const resize = function() {
+    if (display.textContent.length >= 7) {
+        let excess = display.textContent.length - 7;
+        let newFontSize = 46 - (excess * 3.5);
+        console.log(newFontSize);
+        display.style.fontSize = `${newFontSize}px`;
     } else {
-        append('.');
-        updateDisplay();
+        display.style.fontSize = `46px`;
     }
-});
-
-plusMinus.addEventListener('click', () => {
-    output *= -1;
-    updateDisplay();
-});
-
-percent.addEventListener('click', () => {
-    output = (output / 100);
-    updateDisplay();
-})
-
-// helper functions
-function append(n) {
-    if (output === 0) {
-        if (n === '.') {
-            output += n;
-        } else {
-            // shows only 1 zero on screen when output is 0 and user presses 0
-            output = parseInt(output) + parseInt(n);
-        }
-    } else {
-        output += n;
-    }
-}
-
-function getLastKeyClicked() {
-    buttons.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            lastKeyPressed = currKeyPressed;
-            currKeyPressed = e.target.textContent;
-            // console.log(`Last: ${lastKeyPressed} \nCurr: ${currKeyPressed}`);
-        });
-    });
-}
-
-function updateDisplay() {
-    display.textContent = parseFloat(output);
-}
-
-function isOperator(a) {
-    switch(a) {
-        case '+':
-            return true;
-        case '-':
-            return true;
-        case '*':
-            return true;
-        case '/':
-            return true;
-    }
-    return false;
 }
 
 // arithmetic functions
